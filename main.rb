0f5df92ce9c47c50ffc159cf413212a6799136b7
Megan Folsom <mfolsom@gmail.com>
2014-02-05 13:30:49 +0000

    Add sudoku puzzle with blank numbers

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
   seed = (1..9).to_a.shuffle+Array.new(81-9,0)
   sudoku = Sudoku.new(seed.join)
@@ -9,7 +11,19 @@ def random_sudoku
   sudoku.to_s.chars
   end
 
+def puzzle(sudoku)
+  @level = {:easy=>35, :hard=> 55}
+  puzzle = sudoku.dup
+  puzzle_numbers = (0..80).to_a.sample(@level[:easy])
+  puzzle_numbers.each do |index|
+    puzzle[index] = ''
+  end
+  puzzle
+end
+
 get '/' do
-  @current_solution = random_sudoku
+  sudoku = random_sudoku
+  session[:solution] = sudoku
+  @current_solution = puzzle(sudoku)
   erb :index
 end
