4b4f387dc7223798f981ff5a34733a5a8f47621f
Megan Folsom <mfolsom@gmail.com>
2014-02-05 18:41:24 +0000

    add ability to check solution

--- main.rb
+++ main.rb
@@ -1,29 +1,82 @@
 require 'sinatra'
+require 'sinatra/partial'
+require 'rack-flash'
+require_relative './helpers/application'
 require_relative './lib/sudoku'
-require_relative './lib/cell'
 
 enable :sessions
+set :session_secret, "I'm the secret key to sign the cookie"
+use Rack::Flash
+set :partial_template_engine, :erb
+
+
 
 def random_sudoku
-  seed = (1..9).to_a.shuffle+Array.new(81-9,0)
+  seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
   sudoku = Sudoku.new(seed.join)
   sudoku.solve!
   sudoku.to_s.chars
-  end
+end
 
 def puzzle(sudoku)
-  @level = {:easy=>35, :hard=> 55}
-  puzzle = sudoku.dup
-  puzzle_numbers = (0..80).to_a.sample(@level[:easy])
-  puzzle_numbers.each do |index|
-    puzzle[index] = ''
+   @level = {:easy => 45, :hard =>55}
+  make_puzzle = sudoku.dup
+    rand_index = (0..80).to_a.sample(@level[:easy])
+    rand_index.each do |index|
+      make_puzzle[index] = 0
+    end
+  make_puzzle
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution]
+  sudoku = random_sudoku
+  session[:solution] = sudoku  
+  session[:puzzle] = puzzle(sudoku)      
+  session[:current_solution] = session[:puzzle]
+end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  if @check_solution
+    flash[:notice] = "Incorrect values are highlighted in yellow"
   end
-  puzzle
+  session[:check_solution] = nil
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+    first_box_index = (i / 3 * 3)
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box| 
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+
+    memo += three_rows_of_three.flatten
+  }
+
 end
 
 get '/' do
-  sudoku = random_sudoku
-  session[:solution] = sudoku
-  @current_solution = puzzle(sudoku)
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
   erb :index
+end
+
+get '/solution' do
+  @puzzle = session[:solution]
+  erb :index
+end
+
+post '/' do
+  cells = box_order_to_row_order(params["cell"])
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
 end
